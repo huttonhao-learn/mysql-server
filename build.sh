434c944d4d9346b15ff85742f7ab35bc7c53648a
huttonhao <huttonhao@163.com>
2022-03-14 22:41:28 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+sudo cmake . -DWITH_DEBUG=1 \
+-DCMAKE_INSTALL_PREFIX=/Users/huttonhao/Project/C_CPP/Learn/mysql-server/work \
+-DMYSQL_DATADIR=/Users/huttonhao/Project/C_CPP/Learn/mysql-server/work/data
+
+
